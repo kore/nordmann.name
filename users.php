98524c53685725bf2c9eecea22a982dd62cec1f2
Kore Nordmann <mail@kore-nordmann.de>
2024-03-03 06:09:13 +0000

    feat: First try at sending toots from RSS

--- users.php
+++ users.php
@@ -27,6 +27,7 @@ $users = [
         'summary' => "Blog posts by @kore@nordmann.name",
         'secret' => 'EBy43xHREueW48',
         'isBot' => true,
+        'rssSource' => 'https://kore-nordmann.de/blog.rss',
     ],
     "kores-photos" => (object) [
         'user' => "kores-photos",
@@ -34,9 +35,29 @@ $users = [
         'summary' => "Photos by @kore@nordmann.name",
         'secret' => 'ZVC5utS3ubWXAu',
         'isBot' => true,
+        'rssSource' => 'https://kore-nordmann.de/photos.rss',
     ],
 ];
 ksort($users);
+
+// Ammend user information
+foreach ($users as $user) {
+    if (file_exists(__DIR__ . '/public/images/' . $user->user . '.png')) {
+        $user->avatar = '/images/' . $user->user . '.png';
+    } else {
+        $user->avatar = '/images/dummy.svg';
+    }
+
+    $user->id = "https://{$server}/users/{$user->user}";
+    $user->type = $user->isBot ?? false ? 'Service' : 'Person';
+
+    $user->dataDirectory = __DIR__ . '/data/' . $user->user;
+    if (!file_exists($user->dataDirectory . '/posts')) {
+        mkdir($user->dataDirectory . '/posts', 0750, true);
+    }
+    $user->followers = file_exists($user->dataDirectory . '/followers.json') ? json_decode(file_get_contents($user->dataDirectory . '/followers.json')) : [];
+    $user->outbox = array_map('json_decode', array_map('file_get_contents', glob($user->dataDirectory . '/posts/*.json')));
+}
 
 return $users;
 
