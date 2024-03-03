a10ececa06da44e9caea144b9f28833cc01dc65d
Kore Nordmann <mail@kore-nordmann.de>
2024-03-03 06:09:33 +0000

    feat: Started rendering toots

--- templates/user.html.php
+++ templates/user.html.php
@@ -4,7 +4,34 @@
 <div class="container">
   <div class="row">
     <strong class="column center"><?=e($user->followers->count ?? 0);?> follower(s)</strong>
+    <strong class="column center"><?=e(count($user->outbox));?> toot(s)</strong>
   </div>
 </div>
-<p><b>@TODO:</b> Posts…</p>
+<ul class="toots">
+<?php foreach ($user->outbox as $post): ?>
+    <li class="toot row">
+        <div class="column">
+            <img src="<?=e($user->avatar)?>" width="64" height="64" alt="<?=e($user->name)?>" />
+        </div>
+        <div class="column">
+            <div class="toot__meta">
+                <span class="toot__author"><?=e($user->name)?></span>
+                <span class="toot__user">@<?=e($user->user)?>@<?=e($server)?></span>
+                <span class="toot__date"><?=e((new \DateTime("@" . ($post->published / 1000)))->format("j. M 'y"))?></span>
+            </div>
+            <div class="toot__content">
+                <?=$post->content->HTML?>
+            </div>
+            <?php if (!empty($post->attachment)): ?>
+            <div class="toot__media">
+                <img
+                    src="<?=e($post->attachment->preview_url ?: $post->attachment->url)?>"
+                    alt="<?=e($post->attachment->description)?>"
+                />
+            </div>
+            <?php endif; ?>
+        </div>
+    </li>
+<?php endforeach; ?>
+</ul>
 <?php include(__DIR__ . '/foot.html.php') ?>
